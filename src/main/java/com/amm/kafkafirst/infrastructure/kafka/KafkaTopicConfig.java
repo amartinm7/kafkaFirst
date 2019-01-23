451e26b7b54bddfbefe86f6fc83916838984731c
antoniomm <antonio.martinmoreno@zooplus.com>
2019-01-23 18:36:01 +0100

    adding a new topic to the messaging system

---
+++ src/main/java/com/amm/kafkafirst/infrastructure/kafka/KafkaTopicConfig.java
@@ -0,0 +1,30 @@
+package com.amm.kafkafirst.infrastructure.kafka;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin(){
+        final Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1(){
+        return new NewTopic("myTopic", 1, (short) 1);
+    }
+}
