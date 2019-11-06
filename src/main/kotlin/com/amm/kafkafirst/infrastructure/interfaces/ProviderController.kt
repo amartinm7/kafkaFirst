ebf94a0eef5e182df18fcbbc6247c00ae3ac28af
antonio.martin <antonio.martin@schibsted.com>
2019-11-06 23:19:35 +0100

    moving java code to kotlin

--- src/main/kotlin/com/amm/kafkafirst/infrastructure/interfaces/ProviderController.kt
+++ src/main/kotlin/com/amm/kafkafirst/infrastructure/interfaces/ProviderController.kt
@@ -1,28 +1,22 @@
-package com.amm.kafkafirst.infrastructure.interfaces;
+package com.amm.kafkafirst.infrastructure.interfaces
 
-import com.amm.kafkafirst.infrastructure.services.ProducerService;
-import com.amm.kafkafirst.infrastructure.kafka.Message;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.amm.kafkafirst.infrastructure.services.ProducerService
+import com.amm.kafkafirst.infrastructure.kafka.Message
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 
-import java.time.LocalDate;
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-public class ProviderController {
-
-    private ProducerService producerService;
-
-    public ProviderController(@Autowired ProducerService producerService){
-        this.producerService = producerService;
-    }
+class ProviderController(@param:Autowired private val producerService: ProducerService) {
 
     @GetMapping("/provider")
-    public ResponseEntity<HttpStatus> index() {
-        final Message message = new Message(200, "Hello World " + LocalDate.now());
-        producerService.sendMessage(message);
-        return new ResponseEntity<>(HttpStatus.OK);
+    fun index(): ResponseEntity<HttpStatus> {
+        val message = Message(200, "Hello World " + LocalDate.now())
+        producerService.sendMessage(message)
+        return ResponseEntity(HttpStatus.OK)
     }
 }
